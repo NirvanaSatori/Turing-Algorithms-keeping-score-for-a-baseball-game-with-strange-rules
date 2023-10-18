d90923e8532030a0e84eef3c6850f2730871db1a
NirvanaSatori <morris24kariuki@gmail.com>
2023-10-18 22:12:01 +0300

    include the code solution

--- algo.js
+++ algo.js
@@ -3,9 +3,28 @@
  * @return {number} - Sum of scores after performing all operations
  */
 
+// const ops = ["5", "2", "C", "D", "+"]
+
 const calPoints = function(ops)  {
     let result = null
-
+    const newOps = ops.map(string => {
+        if(ops.length>=1 && string === "C" ){
+           return ops.pop()
+        }
+        if (ops.length>=1 && string ==="D"){
+            return ops.push(ops[-1]*2)
+        } 
+        if(ops.length>=2 && string ==="+" ){
+            return ops.push(ops[-1] + ops[-2])
+         }
+    })
+    result = newOps.reduce((total, string)=>{
+        let integer = string.split(" ")
+        if (integer !== "C" || "D"){
+           return total += integer
+        }
+    }, 0)
+    return result
 }
 
 let ops = readline().split(" ")
