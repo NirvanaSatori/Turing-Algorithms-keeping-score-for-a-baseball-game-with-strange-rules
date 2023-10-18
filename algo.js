71a44fa276bf2bb72050516dc875df433b3d45a7
NirvanaSatori <morris24kariuki@gmail.com>
2023-10-18 21:28:56 +0300

    add the code template provided

---
+++ algo.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} ops  -  List of operations
+ * @return {number} - Sum of scores after performing all operations
+ */
+
+const calPoints = function(ops)  {
+    let result = null
+
+}
+
+let ops = readline().split(" ")
+
+console.log(calPoints(ops))
